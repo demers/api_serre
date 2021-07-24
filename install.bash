67e17fa2d09c8f5402c8bc81c47b6d6dd98f98bf
F.-Nicola Demers <fndemers@gmail.com>
2021-07-24 10:07:30 -0400

    Installation Mysql

---
+++ install.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Voir tutoriel: https://www.digitalocean.com/community/tutorials/how-to-install-mysql-on-ubuntu-20-04-fr
+
+sudo apt install mysql-server
+sudo mysql_secure_installation
+sudo mysql
+# CREATE USER 'serre'@'localhost' IDENTIFIED BY 'password';
+# Changer "password" par le bon mot de passe.
+# GRANT ALL PRIVILEGES ON *.* TO 'serre'@'localhost' WITH GRANT OPTION;
