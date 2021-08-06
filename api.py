5ee92f301bb1a99732420d01ae2b28ac3e8cebf3
F.-Nicola Demers <fndemers@gmail.com>
2021-08-06 12:07:53 -0400

    Ajout de l'humidité et route capteur1

--- api.py
+++ api.py
@@ -2,6 +2,7 @@
 
 import mysqldb
 from flask import *
+import datetime
 
 def getDB():
     return mysqldb.MYSQLDB.getInstance()
@@ -9,20 +10,38 @@ def getDB():
 def dbInitialize():
     query = "DROP TABLE IF EXISTS Temperature;"
     getDB().runUpdateQuery(query)
-    query = "CREATE TABLE Temperature (Temperature_id INT NOT NULL AUTO_INCREMENT, Senseur INT, Temp FLOAT, Date_temp DATETIME, CONSTRAINT temp_pk PRIMARY KEY (Temperature_id));"
+    query = "DROP TABLE IF EXISTS Humidite;"
     getDB().runUpdateQuery(query)
-    return 'Temperature'
+    query = "CREATE TABLE Temperature (Temperature_id INT NOT NULL AUTO_INCREMENT, Capteur INT, Temp FLOAT, Date_capteur DATETIME, CONSTRAINT temp_pk PRIMARY KEY (Temperature_id));"
+    getDB().runUpdateQuery(query)
+    query = "CREATE TABLE Humidite (Humidite_id INT NOT NULL AUTO_INCREMENT, Capteur INT, Hum FLOAT, Date_capteur DATETIME, CONSTRAINT hum_pk PRIMARY KEY (Humidite_id));"
+    getDB().runUpdateQuery(query)
+    return 'Temperature, Humidite'
 
 def getTemperatureFromDB():
     query = "SELECT * FROM Temperature;"
     return getDB().runSelectQuery(query)
 
-def getTemperatureSenseurFromDB(senseur_id):
-    query = "SELECT * FROM Temperature WHERE Senseur = " + senseur_id + ";"
+def getHumiditeFromDB():
+    query = "SELECT * FROM Humidite;"
     return getDB().runSelectQuery(query)
 
-def putTemperatureToDB(senseur, temperature):
-    query = "INSERT INTO Temperature (Senseur, Temp, Date_temp) VALUES (" + senseur + ", " + temperature + ", NOW());"
+def getTemperatureSenseurFromDB(capteur_id):
+    query = "SELECT * FROM Temperature WHERE Capteur = " + capteur_id + ";"
+    return getDB().runSelectQuery(query)
+
+def putTemperatureToDB(capteur, temperature, date):
+    # query = "INSERT INTO Temperature (Capteur, Temp, Date_capteur) VALUES (" + capteur + ", " + temperature + ", NOW());"
+    query = "INSERT INTO Temperature (Capteur, Temp, Date_capteur) VALUES (" + capteur + ", " + temperature + ", " +  date + ");"
+    getDB().runUpdateQuery(query)
+
+def getHumiditeSenseurFromDB(capteur_id):
+    query = "SELECT * FROM Humidite WHERE Capteur = " + capteur_id + ";"
+    return getDB().runSelectQuery(query)
+
+def putHumiditeToDB(capteur, humidite, date):
+    # query = "INSERT INTO Humidite (Capteur, Hum, Date_capteur) VALUES (" + capteur + ", " + humidite + ", NOW());"
+    query = "INSERT INTO Humidite (Capteur, Hum, Date_capteur) VALUES (" + capteur + ", " + humidite + ", " + date + ");"
     getDB().runUpdateQuery(query)
 
 def representsInt(s):
@@ -55,14 +74,15 @@ def route_initialize():
 
 @app.route('/temperatures', methods=['POST'])
 def route_temperatures_post():
-    senseur_id = request.form.get('senseur_id')
+    capteur_id = request.form.get('capteur_id')
     temperature = request.form.get('temp')
     if (not representsInt(senseur_id)) or (not representsFloat(temperature)):
-        reponse = jsonify({'Erreur': 'senseur_id ou temp ne sont pas des entiers.'})
+        reponse = jsonify({'Erreur': 'capteur_id ou temp ne sont pas des entiers.'})
     else:
-        putTemperatureToDB(senseur_id, temperature)
+        now = datetime.datetime.now()
+        putTemperatureToDB(senseur_id, temperature, now.strftime('%Y-%m-%d %H:%M:%S'))
         valeurs_enr = dict()
-        valeurs_enr['senseur_id'] = senseur_id
+        valeurs_enr['capteur_id'] = capteur_id
         valeurs_enr['temp'] = temperature
         reponse = jsonify({'Valeurs sauvegardées': valeurs_enr})
     return reponse
@@ -73,10 +93,54 @@ def route_temperatures_get():
     reponse_records = getTemperatureFromDB()
     json_return = dict()
     for row in reponse_records:
-        json_return[row[0]] = { 'Senseur ID': row[1],
+        json_return[row[0]] = { 'Capteur ID': row[1],
                                'Température': row[2],
                                'Date': row[3] }
     return jsonify({'Liste des températures': json_return})
+
+@app.route('/humidites', methods=['POST'])
+def route_humidites_post():
+    capteur_id = request.form.get('capteur_id')
+    humidite = request.form.get('hum')
+    if (not representsInt(capteur_id)) or (not representsFloat(humidite)):
+        reponse = jsonify({'Erreur': 'capteur_id ou hum ne sont pas des entiers.'})
+    else:
+        now = datetime.datetime.now()
+        putHumiditeToDB(capteur_id, humidite, now.strftime('%Y-%m-%d %H:%M:%S'))
+        valeurs_enr = dict()
+        valeurs_enr['capteur_id'] = capteur_id
+        valeurs_enr['hum'] = temperature
+        reponse = jsonify({'Valeurs sauvegardées': valeurs_enr})
+    return reponse
+
+
+@app.route('/humidites', methods=['GET'])
+def route_humidites_get():
+    reponse_records = getHumiditeFromDB()
+    json_return = dict()
+    for row in reponse_records:
+        json_return[row[0]] = { 'Capteur ID': row[1],
+                               'Humidité': row[2],
+                               'Date': row[3] }
+    return jsonify({'Liste des humidités': json_return})
+
+@app.route('/capteur1', methods=['POST'])
+def route_temperatures_post():
+    temperature = request.form.get('temp')
+    humidite = request.form.get('hum')
+    capteur_id = '1'
+    if (not representsFloat(temperature)) or (not representsFloat(humidite)):
+        reponse = jsonify({'Erreur': 'capteur_id ou temp ne sont pas des entiers.'})
+    else:
+        now = datetime.datetime.now()
+        putTemperatureToDB(capteur_id, temperature, now.strftime('%Y-%m-%d %H:%M:%S'))
+        putHumiditeToDB(capteur_id, humidite, now.strftime('%Y-%m-%d %H:%M:%S'))
+        valeurs_enr = dict()
+        valeurs_enr['capteur_id'] = capteur_id
+        valeurs_enr['temp'] = temperature
+        valeurs_enr['hum'] = humidite
+        reponse = jsonify({'Valeurs sauvegardées': valeurs_enr})
+    return reponse
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port = 8080)
