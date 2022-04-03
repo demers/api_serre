b2bcd5de0777a9e34b085d7eb9beac0e5f6731a1
F.-Nicola Demers <fndemers@gmail.com>
2022-04-03 15:59:38 -0400

    Ajout test de 3 à 8 capteurs

--- test_api.bash
+++ test_api.bash
@@ -35,3 +35,46 @@ http -f localhost:8080/capteur2 temp=40.2 hum=90.0
 sleep 1
 echo "Les valeurs sont bien dans la BD Mysql..."
 http -f localhost:8080/capteur2
+
+sleep 1
+echo "La température et l'humidité du capteur 3 est à 50.2 et 80.0..."
+http -f localhost:8080/capteur3 temp=50.2 hum=80.0
+sleep 1
+echo "Les valeurs sont bien dans la BD Mysql..."
+http -f localhost:8080/capteur3
+
+sleep 1
+echo "La température et l'humidité du capteur 4 est à 60.2 et 70.0..."
+http -f localhost:8080/capteur4 temp=60.2 hum=70.0
+sleep 1
+echo "Les valeurs sont bien dans la BD Mysql..."
+http -f localhost:8080/capteur4
+
+sleep 1
+echo "La température et l'humidité du capteur 5 est à 70.2 et 60.0..."
+http -f localhost:8080/capteur5 temp=70.2 hum=60.0
+sleep 1
+echo "Les valeurs sont bien dans la BD Mysql..."
+http -f localhost:8080/capteur5
+
+sleep 1
+echo "La température et l'humidité du capteur 6 est à 80.2 et 50.0..."
+http -f localhost:8080/capteur6 temp=80.2 hum=50.0
+sleep 1
+echo "Les valeurs sont bien dans la BD Mysql..."
+http -f localhost:8080/capteur6
+
+sleep 1
+echo "La température et l'humidité du capteur 7 est à 90.2 et 40.0..."
+http -f localhost:8080/capteur7 temp=90.2 hum=40.0
+sleep 1
+echo "Les valeurs sont bien dans la BD Mysql..."
+http -f localhost:8080/capteur7
+
+sleep 1
+echo "La température et l'humidité du capteur 8 est à 100.2 et 30.0..."
+http -f localhost:8080/capteur8 temp=100.2 hum=30.0
+sleep 1
+echo "Les valeurs sont bien dans la BD Mysql..."
+http -f localhost:8080/capteur8
+
