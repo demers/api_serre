833af8d39a189fc37058f1a0e616c0c10dc093e4
F.-Nicola Demers <fndemers@gmail.com>
2022-04-10 20:00:37 -0400

    Ajout echo sur les requêtes

--- test_api.bash
+++ test_api.bash
@@ -11,43 +11,55 @@ http -f localhost:8080/saturations
 
 sleep 1
 echo "La température du capteur 45 est à 32.2..."
+echo Requête: http -f localhost:8080/temperatures capteur_id=45 temp=32.2
 http -f localhost:8080/temperatures capteur_id=45 temp=32.2
 sleep 1
 echo "Les valeurs sont bien dans la BD Mysql..."
+echo Requête: http -f localhost:8080/temperatures
 http -f localhost:8080/temperatures
 
 sleep 1
 echo "L'humidité du capteur 45 est à 50.2..."
+echo Requête: http -f localhost:8080/humidites capteur_id=45 hum=50.2
 http -f localhost:8080/humidites capteur_id=45 hum=50.2
 sleep 1
 echo "Les valeurs sont bien dans la BD Mysql..."
+echo Requête: http -f localhost:8080/humidites
 http -f localhost:8080/humidites
 
 sleep 1
 echo "La température et l'humidité du capteur 1 est à 30.2 et 100.0..."
+echo Requête: http -f localhost:8080/capteur1 temp=30.2 hum=100.0
 http -f localhost:8080/capteur1 temp=30.2 hum=100.0
 sleep 1
 echo "Les valeurs sont bien dans la BD Mysql..."
+echo Requête: http -f localhost:8080/capteur1
 http -f localhost:8080/capteur1
 
 sleep 1
 echo "La température et l'humidité du capteur 2 est à 40.2 et 90.0..."
+echo Requête: http -f localhost:8080/capteur2 temp=40.2 hum=90.0
 http -f localhost:8080/capteur2 temp=40.2 hum=90.0
 sleep 1
 echo "Les valeurs sont bien dans la BD Mysql..."
+echo Requête: http -f localhost:8080/capteur2
 http -f localhost:8080/capteur2
 
 sleep 1
 echo "La température et l'humidité du capteur 3 est à 50.2 et 80.0..."
+echo Requête: http -f localhost:8080/saturations capteur_id=3 sat=50.2
 http -f localhost:8080/saturations capteur_id=3 sat=50.2
 sleep 1
 echo "Les valeurs sont bien dans la BD Mysql..."
+echo Requête: http -f localhost:8080/saturations
 http -f localhost:8080/saturations
 
 sleep 1
 echo "La température et l'humidité du capteur 4 est à 60.2 et 70.0..."
+echo Requête: http -f localhost:8080/saturations capteur_id=4 sat=60.2
 http -f localhost:8080/saturations capteur_id=4 sat=60.2
 sleep 1
 echo "Les valeurs sont bien dans la BD Mysql..."
+echo Requête: http -f localhost:8080/saturations
 http -f localhost:8080/saturations
 
